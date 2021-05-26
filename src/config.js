60cb7294f37ae08f5d4c9d081f97c5df8d2e3aa3
Hisashin7331 <hiszaszin@gmail.com>
2021-05-26 16:22:18 +0200

    Fixed api url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    apiURL: 'https://quizzy-dbserver.herokuapp.com/api',
+    apiURL: 'https://quizzy-db.herokuapp.com/api',
     templateID: 'template_xm0yl8r',
     serviceID: 'service_04cxlcu',
     userID: 'user_eu2xVs25l5ZqMyIpbGQ1R',
